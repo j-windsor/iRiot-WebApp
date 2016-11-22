7c1f6d2023a0b01b2cc840f612b577e191b09135
jwindsor <jayhwindsor@gmail.com>
2016-11-22 16:50:58 -0500

    hub status monitor

--- hubs/models.py
+++ hubs/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 from rooms.models import Room
 from django.contrib.auth.models import User
+import boto3
+import botocore
 
 # Create your models here.
 class Hub(models.Model):
@@ -10,3 +12,7 @@ class Hub(models.Model):
     owner = models.ForeignKey(User)
     room = models.ForeignKey(Room, blank=True, null=True)
     serial_number = models.CharField(max_length=30, unique=True)
+    def is_updated(self):
+        client = boto3.client('iot-data', region_name='us-east-1')
+        response = client.get_thing_shadow(thingName=self.serial_number)
+        return "\"delta\":" not in response['payload'].read()
