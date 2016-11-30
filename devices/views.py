564a74d21af67fcf16943cecd9433b078303fdba
jwindsor <jayhwindsor@gmail.com>
2016-11-30 14:10:45 -0500

    random number on back of prontohex

--- devices/views.py
+++ devices/views.py
@@ -90,7 +90,7 @@ def remove_device(request, device_id, room_id):
 def send_function(request, function_id):
     client = boto3.client('iot-data', region_name='us-east-1')
     func = Function.objects.get(id=function_id)
-    payload = {"state":{"desired":{"prontohex": func.sendhex, "random":random.random()}}}
+    payload = {"state":{"desired":{"prontohex": func.sendhex+"d"+str(random.random())}}}
     if func.device.room.owner != request.user:
         HttpResponse("f")
     try:
